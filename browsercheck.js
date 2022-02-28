02e1be1866f4794d1ab241ad12459f01ca72f4f0
AlexBelostozky <belostozky1994@mail.ru>
2022-03-01 00:48:31 +0300

    browsercheck fix

--- browsercheck.js
+++ browsercheck.js
@@ -5,8 +5,8 @@ console.log(header.classList);
 console.log(info);
 
 if (info.includes('Safari/605.1.15')) {
-  header.classList.add('for-safari');
+  header.classList.add('safari-page-header');
 } else {
-  header.classList.remove('for-safari');
+  header.classList.remove('safari-page-header');
 }
 console.log(header.classList);
