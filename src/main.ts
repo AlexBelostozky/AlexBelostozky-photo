ec732a7613172a3be42bc77a59006853bfbb9f15
Alex Belostozky <belostozky1994@mail.ru>
2025-01-03 23:50:44 +0500

    comment firebase

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '../router';
-import { firebaseApp, firebaseAnalytics, db } from "./fireBaseConfig";
-import '@/assets/styles/main.sass'
+// import { firebaseApp, firebaseAnalytics, db } from "./fireBaseConfig";
+// import '@/assets/styles/main.sass'
 
 const app = createApp(App);
 
 app.use(router);
 
-app.provide('firebaseApp', firebaseApp);
-app.provide('firebaseAnalytics', firebaseAnalytics);
-app.provide('db', db);
+// app.provide('firebaseApp', firebaseApp);
+// app.provide('firebaseAnalytics', firebaseAnalytics);
+// app.provide('db', db);
 
 app.mount('#app');
