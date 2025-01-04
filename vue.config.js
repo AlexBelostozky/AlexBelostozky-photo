74efc1cb04476e0c6cdf79286cd5cffc2cfb658c
Alex Belostozky <belostozky1994@mail.ru>
2025-01-04 10:36:30 +0500

    fix favicons for production

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
@@ -15,7 +16,20 @@ module.exports = defineConfig({
         '@styles': path.resolve(__dirname, 'src/assets/styles'),
         '@images': path.resolve(__dirname, 'src/assets/images')
       }
-    }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        title: 'AlexBelostozky Photo',
+        publicPath: process.env.NODE_ENV === 'production'
+        ? '/AlexBelostozky-photo/'
+        : '/',
+      })
+    ]
+  },
+
+  chainWebpack: (config) => {
+    config.plugins.delete('html');
   },
 
   css: {
